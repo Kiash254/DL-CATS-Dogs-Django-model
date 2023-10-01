1b42e9f808dd1e9a3521daed07e5de6c588e1d2c
Kiash254 <codewithkiash@gmail.com>
2023-10-01 21:07:39 +0300

    added new urls file

--- predctive_model/models.py
+++ predctive_model/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class YourData(models.Model):
+    field1 = models.CharField(max_length=100)
+    field2 = models.IntegerField(upload_to='images')
+    
+    def __str__(self):
+        return self.field1
+    
